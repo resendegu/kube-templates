af7664f566daa8f9b33f3285d03151bf64bd0920
Guilherme Bernal <guilherme@cubos.io>
2020-01-20 20:00:18 -0300

    export postgres

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export * from "./src/certificate"
 export { env } from "./src/helpers"
 export * from "./src/kubernetes"
+export * from "./src/postgres"
 export * from "./src/replicatedsecret"
 export * from "./src/statelessapp"
 
