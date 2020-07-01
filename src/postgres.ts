6bfd0cc1f91bdc21681417f48f3b2e0d344d07e7
Douglas Gadêlha <dgadelha@live.com>
2020-07-01 16:17:06 -0300

    fix: cannot reassign owned by postgres

--- src/postgres.ts
+++ src/postgres.ts
@@ -503,7 +503,6 @@ export class Postgres {
                               psql -h 127.0.0.1 -U postgres -c 'GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "${user}"' ${database.name}
                               psql -h 127.0.0.1 -U postgres -c 'GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO "${user}"' ${database.name}
                               psql -h 127.0.0.1 -U postgres -c 'GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO "${user}"' ${database.name}
-                              psql -h 127.0.0.1 -U postgres -c 'REASSIGN OWNED BY postgres TO "${user}"' ${database.name}
                             `
                           )
                           .join("\n")}
