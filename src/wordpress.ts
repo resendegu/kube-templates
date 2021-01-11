af5e56d94175ca06369118252d54eba0bc8687fb
Douglas Gadêlha <dgadelha@live.com>
2021-01-11 16:14:22 -0300

    feat: wordpress

---
+++ src/wordpress.ts
@@ -0,0 +1,205 @@
+import { URL } from "url";
+import { generateYaml, parseMemory } from "./helpers";
+import { Ingress, ObjectMeta, Service, StatefulSet } from "./kubernetes";
+
+interface WordPressSpec {
+  version: string;
+  database: {
+    host: string;
+    username: string;
+    password?: string;
+    name?: string;
+  };
+  cpu: {
+    request: string | number;
+    limit: string | number;
+  };
+  memory: {
+    request: string | number;
+    limit: string | number;
+  };
+  ingress: {
+    class?: "public" | "private" | "internal";
+    publicUrl: string;
+    tlsCert?: string;
+    timeout?: number;
+    maxBodySize?: string;
+  };
+}
+
+export class WordPress {
+  constructor(private metadata: ObjectMeta, private spec: WordPressSpec) {}
+
+  get yaml() {
+    const url = new URL(this.spec.ingress.publicUrl);
+
+    return generateYaml([
+      new Service(
+        {
+          name: this.metadata.name,
+          namespace: this.metadata.namespace,
+        },
+        {
+          selector: {
+            app: this.metadata.name,
+          },
+          ports: [
+            {
+              name: "http",
+              port: 80,
+            },
+          ],
+        }
+      ),
+
+      new StatefulSet(this.metadata, {
+        serviceName: this.metadata.name,
+        replicas: 1,
+        selector: {
+          matchLabels: {
+            app: this.metadata.name,
+          },
+        },
+        template: {
+          metadata: {
+            labels: {
+              app: this.metadata.name,
+            },
+          },
+          spec: {
+            automountServiceAccountToken: false,
+            containers: [
+              {
+                name: "wordpress",
+                image: `wordpress:${this.spec.version}`,
+                env: [
+                  {
+                    name: "WORDPRESS_DB_HOST",
+                    value: this.spec.database.host,
+                  },
+                  {
+                    name: "WORDPRESS_DB_USER",
+                    value: this.spec.database.username,
+                  },
+                  {
+                    name: "WORDPRESS_DB_NAME",
+                    value: this.spec.database.name ?? "wordpress",
+                  },
+                  ...(this.spec.database.password
+                    ? [
+                        {
+                          name: "WORDPRESS_DB_PASSWORD",
+                          value: this.spec.database.password,
+                        },
+                      ]
+                    : []),
+                ],
+                imagePullPolicy: "IfNotPresent",
+                ports: [
+                  {
+                    name: "http",
+                    containerPort: 80,
+                  },
+                ],
+                volumeMounts: [
+                  {
+                    mountPath: "/var/www/html",
+                    name: "data",
+                  },
+                ],
+                resources: {
+                  limits: {
+                    cpu: this.spec.cpu.limit,
+                    memory: this.spec.memory.limit,
+                  },
+                  requests: {
+                    cpu: this.spec.cpu.request,
+                    memory: this.spec.memory.request,
+                  },
+                },
+                readinessProbe: {
+                  httpGet: {
+                    path: "/wp-includes/images/blank.gif",
+                    port: 80,
+                  },
+                  initialDelaySeconds: 5,
+                  failureThreshold: 1,
+                  periodSeconds: 3,
+                },
+                livenessProbe: {
+                  httpGet: {
+                    path: "/wp-includes/images/blank.gif",
+                    port: 80,
+                  },
+                  initialDelaySeconds: 5,
+                  failureThreshold: 2,
+                  periodSeconds: 5,
+                },
+              },
+            ],
+          },
+        },
+        volumeClaimTemplates: [
+          {
+            metadata: {
+              name: "data",
+            },
+            spec: {
+              accessModes: ["ReadWriteOnce"],
+              resources: {
+                requests: {
+                  storage: "1Gi",
+                },
+              },
+              storageClassName: process.env.PRODUCTION ? "ssd-regional" : "standard",
+            },
+          },
+        ],
+      }),
+
+      new Ingress(
+        {
+          name: this.metadata.name,
+          namespace: this.metadata.namespace,
+          annotations: {
+            ...(this.spec.ingress.maxBodySize
+              ? {
+                  "nginx.ingress.kubernetes.io/proxy-body-size": parseMemory(this.spec.ingress.maxBodySize).toString(),
+                }
+              : {}),
+            ...(this.spec.ingress.timeout
+              ? {
+                  "nginx.ingress.kubernetes.io/proxy-read-timeout": this.spec.ingress.timeout.toString(),
+                }
+              : {}),
+          },
+        },
+        {
+          tls: this.spec.ingress.tlsCert
+            ? [
+                {
+                  secretName: this.spec.ingress.tlsCert,
+                },
+              ]
+            : [],
+          rules: [
+            {
+              host: url.hostname,
+              http: {
+                paths: [
+                  {
+                    path: url.pathname,
+                    backend: {
+                      serviceName: this.metadata.name,
+                      servicePort: 80,
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        }
+      ),
+    ]);
+  }
+}
