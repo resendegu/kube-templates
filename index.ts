ead26af7f5ba0ee2275abbcfdc40a200c5342052
Douglas Gadêlha <dgadelha@live.com>
2021-01-11 17:41:59 -0300

    fix: add imports

--- index.ts
+++ index.ts
@@ -2,11 +2,15 @@ export * from "./src/certificate";
 export * from "./src/certmanager";
 export { env } from "./src/helpers";
 export * from "./src/kubernetes";
+export * from "./src/mariadb";
 export * from "./src/mongo";
 export * from "./src/mssql";
+export * from "./src/mysql";
 export * from "./src/postgres";
 export * from "./src/redis";
 export * from "./src/replicatedsecret";
 export * from "./src/rethink";
 export * from "./src/statelessapp";
 export * from "./src/staticsite";
+export * from "./src/wordpress";
+
