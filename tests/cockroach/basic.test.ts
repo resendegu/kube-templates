6ecb279f3e5b00d44a3da68c1d5f7b76ea6cdfdf
Ismael Queiroz <ismael.queiroz@cubos.io>
2021-01-26 12:41:58 -0300

    test: add cockroach tests

---
+++ tests/cockroach/basic.test.ts
@@ -0,0 +1,50 @@
+import { Namespace } from "../../src/kubernetes";
+import { Cockroach } from "../../src/cockroach";
+import { apply, deleteObject, randomSuffix, waitPodReady } from "../helpers";
+import { queryCockroach } from "./helpers";
+
+describe("cockroach", () => {
+  const namespace = `test-${randomSuffix()}`;
+
+  beforeAll(() => {
+    apply(
+      new Namespace({
+        name: namespace,
+      })
+    );
+  });
+
+  afterAll(() => {
+    deleteObject("namespace", namespace);
+  });
+
+  test("Create basic database", async () => {
+    apply(
+      new Cockroach(
+        {
+          name: "cockroachdb",
+          namespace,
+        },
+        {
+          cpu: {
+            limit: 1,
+            request: 0,
+          },
+          memory: "64Mi",
+          version: "12",
+          replicas: 1
+        }
+      )
+    );
+
+    waitPodReady(namespace, "cockroachdb-0");
+
+    expect(
+      await queryCockroach(namespace, "cockroachdb-0", "SELECT true AS ok")
+    ).toEqual([
+      {
+        ok: true,
+      },
+    ]);
+  });
+});
