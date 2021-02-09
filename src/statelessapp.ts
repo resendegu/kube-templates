66c1624b63022cce206e36f51f38ab26dc8d6764
Douglas Gadêlha <douglas.gadelha@cubos.io>
2021-02-09 17:14:42 +0000

    Revert "fix: set namespaces limit to podAffinityTerm"
    This reverts commit 99d70592d00adbb32d419e5607378013272493c5

--- src/statelessapp.ts
+++ src/statelessapp.ts
@@ -7,7 +7,7 @@ import {
   ObjectMeta,
   Service,
   Volume,
-  VolumeMount
+  VolumeMount,
 } from "./kubernetes";
 
 interface StatelessAppSpec {
@@ -278,7 +278,6 @@ export class StatelessApp {
                           app: this.metadata.name,
                         },
                       },
-                      namespaces: [this.metadata.namespace],
                       topologyKey: "kubernetes.io/hostname",
                     },
                   },
