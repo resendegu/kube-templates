4cafa0661bc6f22d4ca2779cf554c7b027fee7c6
Douglas Gadêlha <douglas.gadelha@cubos.io>
2021-03-22 14:53:02 +0000

    feat: ALB compatibility

--- src/statelessapp.ts
+++ src/statelessapp.ts
@@ -154,7 +154,7 @@ export class StatelessApp {
           },
           path:
             pathname === "/"
-              ? pathname
+              ? (portSpec.ingressClass === "alb" ? "/*" : pathname)
               : (pathname.endsWith("/")
                   ? pathname.substring(0, pathname.length - 1)
                   : pathname) + "(/|$)(.*)",
