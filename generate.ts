d878fe84b4b513a1551573851f050bde5fdc5e9c
Douglas G <dgadelha@live.com>
2022-02-25 16:18:29 +0000

    fix: lint

--- generate.ts
+++ generate.ts
@@ -1,15 +1,22 @@
+import path from "path";
+
 import axios from "axios";
 import { initial, last } from "lodash";
-import { OpenAPIV2 } from "openapi-types";
-import path from "path";
-import { InterfaceDeclarationStructure, OptionalKind, Project, PropertySignatureStructure } from "ts-morph";
+import type { OpenAPIV2 } from "openapi-types";
+import type {
+  InterfaceDeclarationStructure,
+  OptionalKind,
+  PropertySignatureStructure,
+} from "ts-morph";
+import { Project } from "ts-morph";
 
 interface ApiClassDefinition extends OpenAPIV2.SchemaObject {
   className: string;
 }
 
 const apiSpecs: { [key: string]: string } = {
-  kubernetes: "https://raw.githubusercontent.com/kubernetes/kubernetes/release-1.17/api/openapi-spec/swagger.json",
+  kubernetes:
+    "https://raw.githubusercontent.com/kubernetes/kubernetes/release-1.17/api/openapi-spec/swagger.json",
 };
 
 function mapType(type: string) {
@@ -48,36 +55,40 @@ async function run() {
   const project = new Project();
 
   for (const apiKey of Object.keys(apiSpecs)) {
-    const file = project.createSourceFile(path.join("src", "generated", `${apiKey}.ts`), undefined, {
-      overwrite: true,
-    });
+    const file = project.createSourceFile(
+      path.join("src", "generated", `${apiKey}.ts`),
+      undefined,
+      {
+        overwrite: true,
+      },
+    );
 
-    const apiSpec: OpenAPIV2.Document = (await axios.get(apiSpecs[apiKey])).data;
+    const apiSpec: OpenAPIV2.Document = (await axios.get(apiSpecs[apiKey]))
+      .data;
 
     if (!apiSpec.definitions) {
       return;
     }
 
-    const definitions = Object.keys(apiSpec.definitions).reduce<{ [key: string]: ApiClassDefinition[] }>(
-      (acc, definitionKey) => {
-        const definition = apiSpec.definitions?.[definitionKey];
-        const definitionKeyNs = definitionKey.split(".");
-        const namespace = initial(definitionKeyNs).join(".");
-        const className = last(definitionKeyNs) ?? "";
+    const definitions = Object.keys(apiSpec.definitions).reduce<{
+      [key: string]: ApiClassDefinition[];
+    }>((acc, definitionKey) => {
+      const definition = apiSpec.definitions?.[definitionKey];
+      const definitionKeyNs = definitionKey.split(".");
+      const namespace = initial(definitionKeyNs).join(".");
+      const className = last(definitionKeyNs) ?? "";
 
-        if (!acc.hasOwnProperty(namespace)) {
-          acc[namespace] = [] as ApiClassDefinition[];
-        }
+      if (!acc.hasOwnProperty(namespace)) {
+        acc[namespace] = [] as ApiClassDefinition[];
+      }
 
-        acc[namespace].push({
-          ...definition,
-          className,
-        });
+      acc[namespace].push({
+        ...definition,
+        className,
+      });
 
-        return acc;
-      },
-      {},
-    );
+      return acc;
+    }, {});
 
     for (const definitionKey of Object.keys(definitions)) {
       const definition = definitions[definitionKey];
@@ -87,22 +98,30 @@ async function run() {
       });
 
       namespace.addInterfaces(
-        definition.flatMap<OptionalKind<InterfaceDeclarationStructure>>(classDef => ({
-          docs: classDef.description ? [classDef.description.replace(/\*/gu, "-")] : [],
-          name: classDef.className,
-          isExported: true,
-          properties: Object.keys(classDef.properties ?? {}).flatMap<OptionalKind<PropertySignatureStructure>>(
-            propName => {
-              const propDef = classDef.properties?.[propName] as OpenAPIV2.SchemaObject;
+        definition.flatMap<OptionalKind<InterfaceDeclarationStructure>>(
+          classDef => ({
+            docs: classDef.description
+              ? [classDef.description.replace(/\*/gu, "-")]
+              : [],
+            name: classDef.className,
+            isExported: true,
+            properties: Object.keys(classDef.properties ?? {}).flatMap<
+              OptionalKind<PropertySignatureStructure>
+            >(propName => {
+              const propDef = classDef.properties?.[
+                propName
+              ] as OpenAPIV2.SchemaObject;
 
               return {
-                docs: propDef.description ? [propDef.description.replace(/\*/gu, "-")] : [],
+                docs: propDef.description
+                  ? [propDef.description.replace(/\*/gu, "-")]
+                  : [],
                 name: propName.replace(/-/gu, "_"),
                 type: getType(propDef),
               };
-            },
-          ),
-        })),
+            }),
+          }),
+        ),
       );
     }
 
