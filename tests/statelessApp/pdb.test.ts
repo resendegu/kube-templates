9d3d42da82ddc3ef6bc561f66052077be2cef06a
Arthur Passos <arthur.opassos@gmail.com>
2024-09-30 23:50:54 -0300

    feat: pdb

---
+++ tests/statelessApp/pdb.test.ts
@@ -0,0 +1,48 @@
+import { basicAppMetadataConfig, basicAppSpecsConfig } from "./helper";
+import { StatelessApp } from "../../src/statelessapp";
+
+describe("statelessApp", () => {
+  it("should throw an error when minAvailable is bigger than the number of replicas", () => {
+    const app = new StatelessApp(basicAppMetadataConfig, {
+      ...basicAppSpecsConfig,
+      replicas: 2,
+      minAvailable: 3,
+    });
+
+    expect(() => app.yaml).toThrow(
+      "Minimum available replicas cannot be greater than the number of replicas",
+    );
+  });
+
+  it("should throw an error when minAvailable is bigger than the number of replicas on autoscalling", () => {
+    const app = new StatelessApp(basicAppMetadataConfig, {
+      ...basicAppSpecsConfig,
+      replicas: [1, 5],
+      minAvailable: 2,
+    });
+
+    expect(() => app.yaml).toThrow(
+      "Minimum available replicas cannot be greater than the number of replicas",
+    );
+  });
+
+  it("should not throw an error when minAvailable is smaller than the number of replicas", () => {
+    const app = new StatelessApp(basicAppMetadataConfig, {
+      ...basicAppSpecsConfig,
+      replicas: 2,
+      minAvailable: 1,
+    });
+
+    expect(() => app.yaml).not.toThrow();
+  });
+
+  it("should not throw an error when minAvailable is smaller than the number of replicas on autoscalling", () => {
+    const app = new StatelessApp(basicAppMetadataConfig, {
+      ...basicAppSpecsConfig,
+      replicas: [3, 5],
+      minAvailable: 2,
+    });
+
+    expect(() => app.yaml).not.toThrow();
+  });
+});
